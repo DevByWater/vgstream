9c9435081ac5a6d1488b7532df0ef7304910fe9f
devbywater <egaraudy@gmail.com>
2017-08-22 11:30:18 -0600

    add firebase config

---
+++ src/settings.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase'
+
+export const fbConfig = {
+    apiKey: "AIzaSyCUAj-RFlVnthLeyp5NxmoiOwtyDDDL2rM",
+    authDomain: "vgstream-28753.firebaseapp.com",
+    databaseURL: "https://vgstream-28753.firebaseio.com"
+}
