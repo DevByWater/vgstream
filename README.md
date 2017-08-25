8fe1e5d086cf30049f72fe610b0df1499303c34c
devbywater <egaraudy@gmail.com>
2017-08-24 18:35:45 -0600

    move files from client folder

---
+++ README.md
@@ -0,0 +1,2 @@
+# VG_STREAM
+Stream and share favorite videos using the Twitch API
---
+++ src/components/streams/StreamIndex.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class StreamIndex extends Component{
+    
+    render(){
+
+    }
+}
+
+export default StreamIndex;
